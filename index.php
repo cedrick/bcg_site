8aa75dd0a874e924d320360b02a6932f60c2c7af
cedrick macatangay <cedrickmacatangay@gmail.com>
2011-11-29 19:44:18 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,335 @@
+<html>
+<head>
+<title>Big City Guide Info</title>
+<link rel="shortcut icon" href="templates/images/favicon.ico" type="image/x-icon" />
+<link rel="stylesheet" href="library/ui-lightness/jquery-ui.css" />
+<script type="text/javascript" src="library/jquery.js"></script>
+<script type="text/javascript" src="library/jquery-ui.min.js"></script>
+<script type="text/javascript">
+//$(document).ready(function(){
+
+	
+	
+	//$("#but").click(function(){
+	
+		//$(function() {
+			// a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
+			//$( "#box" ).dialog( "destroy" );
+		
+			//$( "#box" ).dialog({
+				//height: 140,
+				//modal: true
+			//});
+		//});
+	//});
+//});
+
+
+</script>
+</head>
+<body>
+	
+<?php include ("templates/style.css"); ?>
+<?php include ("templates/template.php"); ?>
+<div class="liner">
+<?php include ("templates/navigation.php"); ?>
+</div>
+	
+<center>
+<div class="content">
+<div class ="sub_head">
+	<table align ="left">
+		<tr>
+			<td>
+				<h1> <font color = "#773A7F">BigCityGuide</font></h1>
+			</td>
+		</tr>
+	</table>
+</div>
+<div class = "line_big"></div>
+<table align ="left">
+	<tr>
+		<td>
+			<h3><font color = "#808080">Information</font></h3>
+		</td>
+	</tr>
+</table>
+<br><br>
+<div class = "sub_content_what">
+	<table align ="left" bgcolor = "#F4F6F5">
+		<tr>
+			<td>
+				<b>What Does it Do?</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				It allows visitors who are new to a city to locate local businesses such as restaurants & bars.
+It also allows you to plan a trip in advance – which hotel would you like to stay at, which restaurants would you like to eat at, which attractions would you like to go and see etc.
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>What Type of Businesses are on Big City Guide?</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<font color ="#808080">There are 30 categories which are listed below;</font><br>
+				1.	<i>Restaurants – Italian, Indian, Chinese & Bistro</i><br>
+				2.	ATM Machines<br>
+				3.	Car Parks<br>
+				4.	<i>Attractions</i><br>
+				5.	Petrol Stations<br>
+				6.	Train Stations<br>
+				7.	Taxis<br>
+				8.	Cinemas<br>
+				9.	<i>Cafes & Coffee shops</i><br>
+				10.	Bowling<br>
+				11.	<i>Fast Food</i><br>
+				12.	<i>Hotels</i><br>
+				13.	<i>Bed & Breakfasts</i><br>
+				14.	<i>Bars</i><br>
+				15.	<i>Nightclubs</i><br>
+				16.	Shopping Malls<br>
+				17.	Supermarkets<br>
+				18.	Clothes Shops<br>
+				19.	Electronics<br>
+				20.	<i>Theatres</i><br>
+				21.	Airports<br>
+				22.	Car Rental<br>
+				23.	<i>Caravan Sites</i><br>
+				24.	Swimming Pools<br>
+				25.	Gyms<br>
+				26.	<i>Hair Salons</i><br>
+				27.	Banks<br>
+				28.	Police Stations<br>
+				29.	Hospitals<br>
+				30.	<i>Wine Merchants & Delicatessens</i><br><br>
+				<font color ="#808080">Those in italics are the categories most likely to subscribe initially and we would be looking for about 10 per category.</font>
+
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>How Do Visitors Use Big City Guide?</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Visitors click on a category to locate a business, they can then click on the business to reveal the phone number, access the website, get directions or view the business information including its latest tweet.
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>What Is My Cloud?</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				The My Cloud feature allows visitors to plan their itinery in advance.  They can decide which hotel they would like to stay in, which attractions and restaurants they would like to visit etc.  These can then be stored in advance of the visit.
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>What Type of Phones?</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Big City Guide will currently run on Apple and Android based smartphones.  RIM touchscreens will be supported from September, however, we see Apple & Android being the dominant market players going forward.
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>How Do I get Big City Guide on My Phone?</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Using your smartphone you can download it from the UK app store or from www.bigcityguide/mobile.  We have submitted Big City Guide to the Android Market in the UK and expect it to be available by the end of August.  In the mean time Android users can download from the website.
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>How Much Does it Cost?</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Big City Guide is free to users to download.  Businesses are expected to pay an annual subscription of £25 in the UK.
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>Will Big City Guide be Launched Outside the UK?</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				In the Autumn we will launch in the USA, Canada, Singapore, Australia, New Zealand & South Africa – 350 extra cities. 
+In the winter we will launch in Europe – another 350 cities
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>Telesales Process</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				1.	Initial call to introduce the concept, encourage them to download Big City Guide and send an initial email informing about the product.<br><br>
+				2.	Follow up call to gain sign up agreement.  Complete some of the business data such as Name, address, phone number, contact etc. and submit to BCG.<br><br>
+				3.	A unique login & password is then created for the business. This together with the business name is copied into a final email which is sent to the business.  This final email contains 2 links.  The first to complete their business details – business description, opening hours & twitter feed,  the second is a link to paypal so they can complete their payment.<br><br>
+				4.	Some businesses will be national or regional in nature and it may be better to deal with them on that basis.<br><br>
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>What about Future Enhancements?</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				In the next 6 months we will release version 1.1.  This will give businesses the ability to upload PDF files and Video, for example a restaurant could upload its menu or special offers.  Their will also be an events button were businesses can post events.
+				We will introduce foreign language versions as we expand into the Non English speaking world.
+				Version 1.2 which will come in mid 2012 will have its own advertising platform so subscribers can advertise.
+				Naturally these enhancements will provide future Telesales opportunities.
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>Big City Guide FAQ’s</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				1.	What type of phones can use BCG?<br>
+				<font color = "#135FF7">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Smartphones running the apple and android operating systems.<br>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;It runs better on newer phones with bigger screens.<br><br></font>
+				
+				2.	Geographical Scope<br>
+				<font color = "#135FF7">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BCG is available in 48 cities across the UK.  It is about to be launched in USA, Canada, New Zealand, South Africa, Australia & Singapore.<br><br></font>
+				
+				3.	How can it be downloaded?<br>
+				<font color = "#135FF7">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;It can be downloaded from bigcityguide.co.uk/mobile, the uk app store and the android market.<br><br></font>
+				
+				4.	I have a hotel which is also a restaurant open to the public, can I be listed in more than one category.<br>
+				<font color = "#135FF7">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Yes, you can be listed in a maximum of 2 categories.<br><br></font>
+				
+				5.	I have 2 restaurants in this city, how much would I have to pay?<br>
+				<font color = "#135FF7">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You would have to pay £25 per restaurant<br><br></font>
+				
+				6.	I have 5 hotels, do you offer discounts?<br>
+				<font color = "#135FF7">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Unfortunately not, we feel £25 per business, per year represents good value.<br></font>
+			</td>
+		</tr>
+	</table>
+</div>
+<?php include ("templates/footer.php"); ?>
+
+</div>
+</center>
+</body>
+</html>
+

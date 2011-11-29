8b30d44d427266cb70b20089673b7abf297b3a90
cedrick macatangay <cedrickmacatangay@gmail.com>
2011-11-29 19:44:57 +0800

    first commit

---
+++ features.php
@@ -0,0 +1,481 @@
+<html>
+<head>
+<title>Big City Guide Features & Benefits</title>
+<link rel="shortcut icon" href="templates/images/favicon.ico" type="image/x-icon" />
+<link rel="stylesheet" href="library/ui-lightness/jquery-ui.css" />
+<script type="text/javascript" src="library/jquery.js"></script>
+<script type="text/javascript" src="library/jquery-ui.min.js"></script>
+<script type="text/javascript">
+//$(document).ready(function(){
+
+	
+	
+	//$("#but").click(function(){
+	
+		//$(function() {
+			// a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
+			//$( "#box" ).dialog( "destroy" );
+		
+			//$( "#box" ).dialog({
+				//height: 140,
+				//modal: true
+			//});
+		//});
+	//});
+//});
+
+
+</script>
+</head>
+<body>
+	
+<?php include ("templates/style.css"); ?>
+<?php include ("templates/template.php"); ?>
+<div class="liner">
+<?php include ("templates/navigation.php"); ?>
+</div>
+	
+<center>
+<div class="content">
+<div class ="sub_head">
+	<table align ="left">
+		<tr>
+			<td>
+				<h1> <font color = "#773A7F">BigCityGuide</font></h1>
+			</td>
+		</tr>
+	</table>
+</div>
+<div class = "line_big"></div>
+<table align ="left">
+	<tr>
+		<td>
+			<h3><font color = "#808080">Features and Benefits</font></h3>
+		</td>
+	</tr>
+</table>
+<br><br>
+<div class = "sub_content">
+	<table align ="left" bgcolor = "#F4F6F5">
+		<tr>
+			<td>
+				<b>USERS</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				1.) Businesses are listed by category
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	it provides only relevant results
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	users can easily find/get the contact information like the phone number and the  address
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				2.) Listing includes the name, address and phone number
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	they'll know what businesses are near their location, whether its accessible or not
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	contact a business by clicking on the phone number on the listing; no need to memorize and dial the number
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				3.) Link to the website
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	can view additional information about your business (hours of operations, menu, other facilities and services)
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				4.) View latest twitter feed
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	Can view latest updates regarding your offers, promos, deals, etc.
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				5.) Map and visual directions are provided
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	it will be easier for them to get to your location
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	visual directions on how to get to the business from where they are located
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				6.) My cloud feature
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	users can prepare their itinerary by saving the information of the businesses/places they'd like to go to
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				7.) FREE
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	can download the app at no cost
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				8.) Runs on Apple and Android Smartphones
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	bigger market reach
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>SUBSCRIBER</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				1.) Listing includes
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	business name,business address, business description, phone number
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				2.) Link to the website
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	You can provide your potential customers additional information about your business (hours of operations, menu, other facilities and services)
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				3.) Map and directions
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	it will be easier for them to get to your location
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	visual directions on how to get to the business from where they are located
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				4.) Twitter feed
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	Can post latest updates regarding your offers, promos, deals, etc.
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				5.) Unlimited updates
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	Since you will be provided with your own username and password, you can access you listing anytime. update your company information without additional cost
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				6.) 2 different categories
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	A business can be listed under 2 different categories. It’s like having 2 different advertisements for the price of one
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				7.) £25 per year
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	Less expensive compared to how much you spend on your other advertisements.
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	it’s like spending only £ 2 /  month on your advertisement for increased market potential
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	it’s only a fraction of the cost of your usual advertisement
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				8.) No long term commitment
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	you have the option to renew your listing after 1 year
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				9.) 14-day money back guarantee
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;•	you can get your money back if you're not satisfied with the increased business we're getting you within 14 days
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<i><font color ="#808080"> **Please review and incorporate the following in presenting the offer:</font></i>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				BUYING CYCLE<br>
+				REASONS FOR ADVERTISING 3Ps<br>
+				TYPES OF ADVERTISING<br>
+				KNOWN AND UNKNOWN MARKET<br>
+				AFFECTIVE ADVERTISING<br>
+	
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•REACH</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;oAdvertising should reach as many consumers, especially in the business’ target market, as possible. 
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•FREQUENCY</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o	Advertising should appear often to consumers, especially when looking for the business’ product or service.
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•ECONOMY	</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o	Advertising should provide a good return on investment.
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•USAGE</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o	The advertising media should be used by consumers often.
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•CUSTOMER FOLLOW UP</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o	The media should be one in which consumers can easily follow up. 
+			</td>
+		</tr>
+	</table>
+</div>
+<?php include ("templates/footer.php"); ?>
+
+</div>
+</center>
+</body>
+</html>
+

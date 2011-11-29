50f83281cc9902a460f8bc240a9216b4250d0842
cedrick macatangay <cedrickmacatangay@gmail.com>
2011-11-29 19:49:02 +0800

    first commit

---
+++ templates/navigation.php
@@ -0,0 +1,48 @@
+<style type="text/css">
+a:link {
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:hover {
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
+</style>
+<div class = "quick_links">
+      <a href="index.php">
+       <strong>
+         <font color="#FFFBF0"face="Arial Rounded MT Bold" size="3">
+          Home
+         </font>
+       </strong>
+	</a>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     <a href="features.php">
+      <strong>
+        <font color="#FFFBF0" face="Arial Rounded MT Bold" size="3">
+          Features
+        </font>
+      </strong>
+     </a>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="bcg_script.php">
+        <strong>
+          <font color="#FFFBF0" face="Arial Rounded MT Bold" size="3">
+           Script
+          </font>
+        </strong>
+      </a>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="rebuttals.php">
+       <strong>
+         <font color="#FFFBF0" face="Arial Rounded MT Bold" size="3">
+          Rebuttals
+         </font>
+       </strong>
+      </a>
+</div>

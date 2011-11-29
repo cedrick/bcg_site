3fae17fe04c789972aebec7cd6c0f5f724a59f25
cedrick macatangay <cedrickmacatangay@gmail.com>
2011-11-29 19:43:40 +0800

    first commit

---
+++ bcg_script.php
@@ -0,0 +1,423 @@
+<html>
+<head>
+<title>Big City Guide Script</title>
+<link rel="shortcut icon" href="templates/images/favicon.ico" type="image/x-icon" />
+<link rel="stylesheet" href="library/ui-lightness/jquery-ui.css" />
+<script type="text/javascript" src="library/jquery.js"></script>
+<script type="text/javascript" src="library/jquery-ui.min.js"></script>
+<script type="text/javascript">
+//$(document).ready(function(){
+
+	
+	
+	//$("#but").click(function(){
+	
+		//$(function() {
+			// a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
+			//$( "#box" ).dialog( "destroy" );
+		
+			//$( "#box" ).dialog({
+				//height: 140,
+				//modal: true
+			//});
+		//});
+	//});
+//});
+
+
+</script>
+</head>
+<body>
+	
+<?php include ("templates/style.css"); ?>
+<?php include ("templates/template.php"); ?>
+<div class="liner">
+<?php include ("templates/navigation.php"); ?>
+</div>
+	
+<center>
+<div class="content">
+<div class ="sub_head">
+	<table align ="left">
+		<tr>
+			<td>
+				<h1> <font color = "#773A7F">BigCityGuide</font></h1>
+			</td>
+		</tr>
+	</table>
+</div>
+<div class = "line_big"></div>
+<table align ="left">
+	<tr>
+		<td>
+			<h3><font color = "#808080">BigCityGuide<br>Revision 1.1 11/24/2011</font></h3>
+		</td>
+	</tr>
+</table>
+<br><br><br>
+<div class = "sub_content_script">
+	<table align ="left" bgcolor = "#F4F6F5">
+		<tr>
+			<td>
+				<div class = "intro"><b>INTRO</b></div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Hello! This is [agent name]. Could I speak with the owner or manager of [business name] please? (Wait for owner/manager)<br><br>
+Hi there! Could I have your name so I can address you properly? (Obtain name)
+
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<div class = "intro"><b>POC</b></div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Hi, [contact name]! Again, this is [agent name] from Big City Guide. We are a mobile city guidebook that helps business travelers, tourist, locals and students find what they want and what they need instantaneously thru their Smartphone.<br>
+We make a free downloadable application for iPhone and Android users to help them search for locations such as restaurants, café, hotels, bed and breakfast and tourist attractions and <b>[mention their business type]</b>. <br><br>
+
+Anybody would be able to locate businesses closest to them. It will provide them direction on how to get there, know the opening hours, current offers, and view detailed information of each business including contact numbers.<br><br>
+
+We’d like to add [business name] to our database.<br><br>
+
+<b>Talking points: Walk-through app Download</b><br>
+If you don’t mind me asking, do you have an iPhone or an Android? <i>(If yes, walk them through the download and features of the app.) </i><br><br>
+
+Go to your app store/android market and search for "big city guide."<br><br>
+
+You'll see that we are the only result. If you select us, you'll see a description of our app as well as some nice pictures of what the screen would look like. Go ahead and download it. You'll notice that it's free, so users don't have to pay to access your company's information.
+<i>(If no, ask them if they have a computer, then walk them through the features on our website. If no computer and no phone, then just go through the features with them verbally).</i><br><br>
+<b>Features (give benefit statements)</b><br>
+	&nbsp;&nbsp;&nbsp;•	Businesses are listed by category<br>
+	&nbsp;&nbsp;&nbsp;•	Listing includes the name, address and phone number<br>
+	&nbsp;&nbsp;&nbsp;•	Link to the website<br>
+	&nbsp;&nbsp;&nbsp;•	View latest twitter feed<br>
+	&nbsp;&nbsp;&nbsp;•	Map and visual directions are provided<br>
+	&nbsp;&nbsp;&nbsp;•	My cloud feature<br>
+	&nbsp;&nbsp;&nbsp;•	FREE to download for Users<br>
+	&nbsp;&nbsp;&nbsp;•	Unlimited updates for subscribers<br><br>
+
+As I’ve mentioned, we’re marketing ourselves as a free, do everything app and the latest version has only been up for a few months, but we’ve already had thousands of downloads. <br><br>
+
+I’m not sure if you are aware that over 31 million tourists visit the UK every year, and more of them will be coming next year for the Olympics. Are you aware of this, Mr. [DM Name]? 
+
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<div class = "intro"><b>OFFER and PRICE</b></div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				I’d like to boost your business by adding you to our city guide and letting all those tourists know who and where [business name] is.<br><br>
+
+				Adding your business to our database is easy. I can gather important information like opening times, a short description of your business, contact information, 2 keywords that will help users locate you…. <br><br>
+				
+				Once listed, your business profile is accessible to millions of iPhone or Android based Smartphone users who may potentially be your customers. Listing with BigCityGuide is just a fraction of the cost it would take to put up a newspaper or media ad, with much better results. It’s only £25 annually or about 2 pounds and few pence a month. There are no long term contracts with BigCityGuide.<br><br>
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<div class = "intro"><b>CLOSE</b></div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Shall we go ahead and get your profile started?<br><br>
+
+				Once I have created your basic profile, please have you credit card handy. <br><br>
+				
+				I would need the card number and expiration to process your payment. After I’ve processed your payment, we’ll send you an email with login information for you to update your company profile including a twitter feed. Ok?<br><br>
+				
+				<i>Start taking down Business Information, etc…..</i>
+
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<div class = "intro_2"><b>BUSINESS INFORMATION CAPTURE</b></div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<b>Business Information</b><br>
+				&nbsp;&nbsp;&nbsp;•	Location: <br>
+				&nbsp;&nbsp;&nbsp;•	Business Name: <br>
+				&nbsp;&nbsp;&nbsp;•	Building Name/No: <br>
+				&nbsp;&nbsp;&nbsp;•	Street: <br>
+				&nbsp;&nbsp;&nbsp;•	City: <br>
+				&nbsp;&nbsp;&nbsp;•	Postcode: <br>
+				&nbsp;&nbsp;&nbsp;•	Country: <br>
+				&nbsp;&nbsp;&nbsp;•	Phone: <br>
+				&nbsp;&nbsp;&nbsp;•	Email: <br>
+				&nbsp;&nbsp;&nbsp;•	Website: <br>	
+				&nbsp;&nbsp;&nbsp;•	Opening Times: 	<br>
+				&nbsp;&nbsp;&nbsp;•	Business Description:<br> 
+				&nbsp;&nbsp;&nbsp;•	Business Keyword 1: <br>
+				&nbsp;&nbsp;&nbsp;•	Business Keyword 2: <br><br>
+				<b>Contact Information</b><br>
+				&nbsp;&nbsp;&nbsp;•	Contact Person:<br>
+				&nbsp;&nbsp;&nbsp;•	Contact Email:<br>
+				&nbsp;&nbsp;&nbsp;•	Contact Phone:<br>
+								
+
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<div class = "intro"><b>CC VERIFICATION</b></div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Also to show that we have authorization to set up your BigCityGuide listing, we’ve collected your credit card information which is:<br>
+				&nbsp;&nbsp;&nbsp;1.	CC Type : (Visa, MasterCard, American Express, Discover)<br>
+				&nbsp;&nbsp;&nbsp;2.	CC Number :  <br>
+				&nbsp;&nbsp;&nbsp;3.	Expiration date :  (mm/yy)<br>
+				&nbsp;&nbsp;&nbsp;4.	Name that appears on the CC:  <br>
+				Is that correct?<br><br>
+				We’ll be sending you the confirmation email with the details within 24 hours. Once you get it, you can use the username and password immediately. After you’ve saved your profile, you can check the app right away to see [business name]’s Big City Guide entry. And please remember that you can edit your information at any time, to update promotions, or put in more content.<br><br>
+
+				Just to recap, we will be processing GBP25 on the card that you have provided me, and the name that will appear on your credit card bill is Polaris. Before I go, do you have further clarifications? Please don’t hesitate to contact me M-F, 9 to 5 at +442030025941 should you require further assistance <br><br>
+				
+				or<br><br>
+				
+				If you have any questions or would like to follow up at any part of the process, please call our customer care hotline at 01904-236200. Thanks, [customer name]. Have a good day. 
+				We look forward to having you as part of the Big City Guide community.
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>Keywords:</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+			<div class = "key">
+				Restaurant<br>
+				Italian Restaurant<br>
+				Indian Restaurant<br>
+				Chinese Restaurant<br>
+				Bistro<br>
+				Fast Food<br>
+				Tourist Attraction<br>
+				Coffee Shops and Cafes<br>
+				Bars<br>
+				Shopping Centers<br>
+				Hotels<br>
+				Bed and Breakfast<br>
+				Entertainment<br>
+				Public Transport<br>
+				Local Council<br>
+				Emergency Services<br>
+				Caravan and Camp Sites<br>
+				Delicatessens & Wine Merchants<br>
+				Petrol Stations<br>
+				Taxi Companies<br>
+				Cash Machines<br>
+				Car Park<br>
+				Airport<br>
+				Car Rental<br>
+				Nightclubs<br>
+				Banks<br>
+				Supermarkets<br>
+				Clothes Shop<br>
+				Electronic Shops<br>
+				Theatres<br>
+				Cinemas<br>
+				Bowling Alleys<br>
+				Swimming Pools<br>
+				Gyms<br>
+				Police Stations<br>
+				Train Stations<br>
+				Hair Salons<br>
+				Hospitals<br>
+				Bus Stop<br>
+				Take Away<br>
+				Tourist Information<br>
+				Pharmacy<br>
+				Free Wifi<br>
+			</div>				
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>Location:</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<div class = "key_2">
+					York<br>
+					Cambridge<br>
+					Aberdeen<br>
+					Bath<br>
+					Birmingham<br>
+					Bradford<br>
+					Brighton<br>
+					Bristol<br>
+					Canterbury<br>
+					Cardiff<br>
+					Carlisle<br>
+					Chichester<br>
+					Coventry<br>
+					Derby<br>
+					Dundee<br>
+					Durham<br>
+					Edinburg<br>
+					Exeter<br>
+					Glasgow<br>
+					Gloucester<br>
+					Hereford<br>
+					Hull<br>
+					Inverness<br>
+					Lancaster<br>
+					Leeds<br>
+					Leicester<br>
+					Lichfield<br>
+					Lincoln<br>
+					Liverpool<br>
+					Manchester<br>
+					Preston<br>
+					Ripon<br>
+					Salford<br>
+					Salisbury<br>
+					Sheffield<br>
+					Southampton<br>
+					St. Albans<br>
+					Stirling<br>
+					Stoke-on-Trent<br>
+					Sunderland<br>
+					Swansea<br>
+					Wakefield<br>
+					Central London<br>
+					Winchester<br>
+					Wolverhampton<br>
+					Worcester<br>
+					Bournemouth<br>
+				</div><br><br>
+				Step-by-Step info:<br><br>
+
+				Once a business has agreed on phone, add the business by logging in here: <br><br>
+				
+				<a target = "_blank" href = "http://www.bigcityguide.co.uk/BusinessPortal/">http://www.bigcityguide.co.uk/BusinessPortal</a><br><br>
+				
+				( user: chris.leaver, pass: bigcity )<br><br>
+				
+				Add all the * info, press submit, and login details will be emailed.
+			</td>
+	</table>
+</div>
+<?php include ("templates/footer.php"); ?>
+
+</div>
+</center>
+</body>
+</html>
+

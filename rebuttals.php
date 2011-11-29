e597d5921a31ba4d2d81c82e9af3b14b7e2c0e50
cedrick macatangay <cedrickmacatangay@gmail.com>
2011-11-29 19:42:39 +0800

    first commit

---
+++ rebuttals.php
@@ -0,0 +1,510 @@
+<html>
+<head>
+<title>Big City Guide Rebuttals</title>
+<link rel="shortcut icon" href="templates/images/favicon.ico" type="image/x-icon" />
+<link rel="stylesheet" href="library/ui-lightness/jquery-ui.css" />
+<script type="text/javascript" src="library/jquery.js"></script>
+<script type="text/javascript" src="library/jquery-ui.min.js"></script>
+<script type="text/javascript">
+//$(document).ready(function(){
+
+	
+	
+	//$("#but").click(function(){
+	
+		//$(function() {
+			// a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
+			//$( "#box" ).dialog( "destroy" );
+		
+			//$( "#box" ).dialog({
+				//height: 140,
+				//modal: true
+			//});
+		//});
+	//});
+//});
+
+
+</script>
+</head>
+<body>
+	
+<?php include ("templates/style.css"); ?>
+<?php include ("templates/template.php"); ?>
+<div class="liner">
+<?php include ("templates/navigation.php"); ?>
+</div>
+	
+<center>
+<div class="content">
+<div class ="sub_head">
+	<table align ="left">
+		<tr>
+			<td>
+				<h1> <font color = "#773A7F">BigCityGuide</font></h1>
+			</td>
+		</tr>
+	</table>
+</div>
+<div class = "line_big"></div>
+<table align ="left">
+	<tr>
+		<td>
+			<h3><font color = "#808080">Rebuttals</font></h3>
+		</td>
+	</tr>
+</table>
+<br><br>
+<div class = "sub_content_rebuttals">
+	<table align ="left" bgcolor = "#F4F6F5">
+		<tr>
+			<td>
+				<b>Probing Questions:</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;•	Just like your business is listed in the Yellow Page PRINT directory, have you also made sure you’re listed in the ON LINE Yellow Pages?<br>
+				&nbsp;&nbsp;&nbsp;•	How are you currently advertising your business to the online market?<br>
+				&nbsp;&nbsp;&nbsp;•	What line of business are you in?<br>
+				&nbsp;&nbsp;&nbsp;•	How many new customers would it take to pay for the £25 cost of placing your business’s ad on line with BigCityGuide?<br>
+				&nbsp;&nbsp;&nbsp;•	Do you have a website?<br>
+				&nbsp;&nbsp;&nbsp;•	How are you bringing new customers to your website?<br>
+				&nbsp;&nbsp;&nbsp;•	How are customers finding you now?<br>
+				&nbsp;&nbsp;&nbsp;•	How are customers who shop online using their mobile phones finding you?<br>
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>BIGCITYGUIDE – Quick No Interest Rebuttals</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				&nbsp;&nbsp;&nbsp;1.	QNI:     Mr./Ms.___________, Your business is listed in the Yellow Page PRINT <br>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;directory, right?  Just like your business is listed in the PRINTED Yellow Pages, you also need to make sure you’re listed in the mobile internet version of the Yellow Pages.  Let me tell you how easy it is….<br><br>
+
+				&nbsp;&nbsp;2.	QNI - WHAT DOES IT COST?/IS THERE A COST FOR THIS?  <br> 
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;An investment of £25 is all it takes for [business name] to be listed on BigCityGuide in front of your potential customers looking for [business type] in your area.<br>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;For just the price of a cinema or theatre ticket you can have your business listed on the mobile app version of the phone book for 12 months, that’s just a fraction of the cost you’d normally spend to put up a print, tv, or radio ad, and with much better results <br><br>
+				
+				You are in __Field #32_________county, correct?  And that’s where you get most of your customers, right?<br><br>
+				
+				BigCityGuide is a FREE downloadable mobile app for mobile internet users.   For just the price of a cinema or theatre ticket you can have your business listed on the mobile app version of the phone book for 12 months . Potential customers looking for a [business type] like you will see YOUR listing sooner so YOU’LL get the calls.  (Keep in mind that half the people who use the Yellow Pages DON’T have any particular business in mind—they just know what they need.  I’m sure you’d want some of THEM to call you too.)<br><br>
+				
+				QNI - I’VE ALREADY BEEN CALLED ABOUT THIS:    I apologize for that.  Sometimes when businesses have more than 1 phone line, they get more than 1 call. But before I let you go, can I just ask if the person who called you was calling about the BigCityGuide mobile app? And did they review our special offer with you? The reason I ask is that I’d hate to see you miss out.   It really is a great new offer … 
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<font color = "#808080"><b>GENERAL REBUTTALS</b></font>
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>NO COMPUTER/SMARTPHONE</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				You don't need a computer or Smartphone to be on BigCityGuide, lots of our customers don't either.  But many people who could become YOUR customers are using BigCityGuide on their smartphone to find the people they do business with.  For just about the price of a cinema or theatre ticket, you can be listed on BigCityGuide so people who are looking for a business like yours can find you.  And you'd also want to be sure that the people who are recommended to you can locate you easily, right?
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>WORD OF MOUTH / REFERRAL</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Even when a customer is referred to you by word of mouth they usually are only given your NAME.    Often they get more than one referral, too.  They STILL end up going to the directory to get your phone number or address so they can contact you.  You want to make sure they find YOUR listing instead of just a competitor’s right?
+Plus, about 50% of the people who use the directory don't have any specific business name or referral in mind--they just know what they need.  (newcomers, emergency buyers, dissatisfied, infrequent, comparison shoppers, travelers-business or vacation).  You have a right to a share of THAT business too, even if they weren't referred, right?
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>I USE OTHER FORMS OF ADVERTISING</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				It's great that you see the benefits of advertising.  What kind of advertising do you do?<br><br>
+
+Newspaper/Magazine/Flyer/Direct Mail/Commercials
+That's a good way to get your name out there and let people know what you offer.  But that's considered CREATIVE  ADVERTISING.  The prospective buyer may not need your product or service at the time they see your advertising.  Later on, when they DO have the need, the Creative Advertising is no longer in front of them.  So they turn to the Yellow Pages to direct them to a business that has what they're looking for.   So that's why you need to be sure they can find you then.  Otherwise the investment you made in your Creative Advertising could be wasted.  BigCityGuide is an insurance policy that protects the dollars you spend on your other advertising.  Plus with BigCityGuide, you can make unlimited updates on your listing with no additional cost
+
+
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>WE HAVE MORE BUSINESS THAN WE CAN HANDLE</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+			You’re lucky because not every business that I talk to can say that.  But most of the businesses that I’ve worked with like yours tell me that there are certain aspects of your business that are more profitable than others.  So if I could get you more calls, you’d be able to pick and choose the work that you make the most money on.  I’m not talking about bringing in MORE customers, because as you said, you really can’t handle any more than you have.  I’m talking about bringing in different customers.  You’d still be working the same amount of hours, but you’d make more money in the end. And even successful businesses like yours do lose customers—people die or move away or maybe you drop them because they don’t pay on time.  You do have to replace the ones that you lose to keep your business healthy.
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>CAN’T AFFORD</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Advertising on BigCityGuide is a productive expense because it brings the buyer to you at that special moment—when he’s made the decision to buy and wants to know just who has what he’s looking for.  That’s a claim that no other form of advertising can make.  It not only pays for itself quickly, it generates profits that help you pay for some of your other fixed costs like electricity or rent.  BigCityGuide is something you can’t afford to be without.
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>CUSTOMER WORTH</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				May I ask, when you get a new customer, how much would they spend with you?...........................Well, I’m not sure what your profit margin is, but how many new customers would it take to cover an investment of about two to three pounds sterling per month? For just the price of a cinema or theatre ticket you can have your business listed on the mobile app version of the phone book for 12 months.
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>BUSINESS IS TERRIBLE</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				I’m sorry to hear that, but then I’m really glad I called.  It sounds like you need more customers, and BigCityGuide can bring them to you.  For just the price of a cinema or theatre ticket you can have your business listed on the mobile app version of the phone book  and you’ll have access to a whole new group of potential customers for 12 months.<br><br>  
+				
+				I’m on the NATIONAL DO NOT CALL  List:<br>
+				Mr/Mrs._______ I understand your concern.  Since the national do not call list
+				applies to residences and mobile phones, but not business telephone numbers this is probably why you were called.  We could have your number added to our Do Not Call list
+				All right, then we’ll add your number to our Do Not Call list.
+				May I ask, are there any additional numbers for this business that should be added?  {capture any additional numbers}
+				All right, we’ll have those removed too.  Thank you. Good-bye.
+				{Disposition as DNC}<br><br>
+				
+				
+				1.I don’t have a smartphone/computer<br>
+				2.	We’ve been the in business for quite some time, everybody knows us<br>
+				3.	We already have a website / we don’t have a website<br>
+				4.	Is this the same as the city guide map that they hand out on train stations/ airports?<br>
+				5.	We are already on the local/printed directory<br>
+				6.	I use other forms of advertising<br>
+				7.	I need to consult my boss about this first<br>
+				8.	Send me something in the mail to look at<br>
+				9.	Are you selling something?<br>
+				10.	I already have a listing on city guide (competitor)<br>
+				11.	Is the app service provider dependent<br>
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>CUSTOMER SERVICE HOTLINE:</b><br>
+				---01904-236200
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>INBOUND NUMBER:</b><br>
+				--Monday to Friday, 9am-5pm<br>
+				--(+44) 2030025941
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>PERSON TO LOOK FOR:</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				- Owner or Manager of the Business<br>
+				- “I need to speak with the person who can make decisions about your local business directory listing. Who would that be, please?”<br>
+
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>GATE KEEPER - PURPOSE OF THE CALL</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				“I’m calling about increasing [business name’s] visibility to tourists and business travelers in your city.  And I need to speak with the person who can give the final approval on that.”
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>GATE KEEPER - IM NOT INTERESTED</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				“I know that you’re probably called with a lot of offers that sound similar, but if you’ll give me a minute to explain…People are spending more time searching on their mobile phones than ever before and one half of local searches are performed on mobile phones<br>
+				We make a FREE downloadable tourist application for Smartphone users, and we’d like to add 		[business name] to our database. I need to speak with the person who can give the final approval on that.”
+
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>PERSON IN CHARGE OF THE ADVERTISING IS HARDLY EVER IN THE OFFICE (IN AND OUT OF THE OFFICE)</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				“I understand exactly what you mean.  Is there anyone else besides him that can make decisions about the local directory listing for the business?”<br>
+				If No: “Could you just point me in the right direction then.  Will I have better luck getting him first thing in the morning, or perhaps in the late afternoon?”
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+			<td>
+				<b>Corporate Rebuttal</b>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				I know that your corporate office takes care of your national advertising that promotes your brand name and benefits all of your locations.  But I’m not calling about that type of advertising.  I’m calling about local advertising to help you bring more customers to this location, right here in _________.  Do you have any local budget for advertising that you can spend yourselves?<br><br>
+
+				If yes: “may I speak to the person in charge of your local advertising please?”<br>
+				If no: “ok, I’ll take note of that information” – courtesy close<br>
+
+			</td>
+		</tr>
+	</table>
+</div>
+<?php include ("templates/footer.php"); ?>
+
+</div>
+</center>
+</body>
+</html>
+
